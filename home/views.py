d0c7ad909ea3ffa390e5288e2e64834254f621b2
Felipe <zfelipesilveira@gmail.com>
2022-08-11 16:54:22 -0300

    segundo dia linkedin learning

--- home/views.py
+++ home/views.py
@@ -2,9 +2,15 @@ from datetime import datetime
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'home/welcome.html', {'today': datetime.today()})
+
+
+@login_required
+def authorized(request):
+    return render(request, 'home/authorized.html', {})
