b6f6c613b58ab16d4f9f0784c1f2f34378c774ec
Felipe <zfelipesilveira@gmail.com>
2022-08-16 15:42:07 -0300

    login.html

--- home/views.py
+++ home/views.py
@@ -1,12 +1,32 @@
 from datetime import datetime
 
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
 
 # Create your views here.
+
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = '/smart/notes'
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
+
+
+class LogoutInterfaceView(LogoutView):
+    template_name = 'home/logout.html'
+
+
+class LoginInterfaceView(LoginView):
+    template_name = 'home/login.html'
 
 
 class HomeView(TemplateView):
